8d8df09cfcd041807122a0d369675788ae56481c
Ali Ramazanov <toaliramazanov@gmail.com>
2025-10-08 18:53:14 +0400

    add first classroom practice

---
+++ module01/classwork/lesson01/src/Cat.java
@@ -0,0 +1,22 @@
+public class Cat {
+    
+    static void main (String[] args) {
+        
+        int age = 10;
+        
+        Integer abTest = age;
+        
+        long xUB = abTest;
+        
+        System.out.println(abTest);
+        System.out.println(xUB);
+        
+        
+        int x = 300;
+        long y = x;
+        
+        // Casting
+        byte z = (byte) x;
+        System.out.println(z);
+    }
+}
---
+++ module01/classwork/lesson01/src/Exercise.java
@@ -0,0 +1,19 @@
+public class Exercise {
+    
+    static void main () {
+        
+        byte first = 100;
+        Byte second = first;
+        
+        System.out.println(second);
+        
+        float third = 3.94F;
+        double fourth = third;
+        
+        System.out.println(fourth);
+        
+        int ascii = 101;
+        char letter = (char) ascii;
+        System.out.println(letter);
+    }
+}
